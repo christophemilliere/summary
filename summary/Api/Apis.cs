f877ecb6a312cbee0c463208002eb7d732326928
christophemilliere <christophemilliere93@gmail.com>
2014-05-19 12:15:50 +0200

    add methode

--- summary/Api/Apis.cs
+++ summary/Api/Apis.cs
@@ -5,11 +5,30 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Net;
 using System.Windows;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using System.Diagnostics;
 
 namespace summary.Api
 {
     class Apis
     {
+
+        private string blog;
+
+        public string Blog
+        {
+            get { return blog; }
+            set { blog = value; }
+        }
+        private string blogName;
+
+        public string BlogName
+        {
+            get { return blogName; }
+            set { blogName = value; }
+        }
+        
         public void Summary(){
             WebClient client = new WebClient();
             client.DownloadStringCompleted += client_DownloadStringCompleted;
@@ -21,7 +40,12 @@ namespace summary.Api
             if (e.Error == null)
             {
                 string texte = e.Result;
-                MessageBox.Show(texte);
+
+                //MessageBox.Show((string)JsonConvert.DeserializeObject(texte));
+                List<Object> products = JsonConvert.DeserializeObject<List<Object>>(texte);
+                
+                //Console.WriteLine(JsonConvert.SerializeObject(texte));
+     
             }
             else
             {
