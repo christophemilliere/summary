4ded18d61b28e9ffee78cffb10300b2792b452bc
christophemilliere <christophemilliere93@gmail.com>
2014-05-19 10:41:42 +0200

    first commit

---
+++ summary/Api/Apis.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Windows;
+
+namespace summary.Api
+{
+    class Apis
+    {
+        public void Summary(){
+            WebClient client = new WebClient();
+            client.DownloadStringCompleted += client_DownloadStringCompleted;
+            client.DownloadStringAsync(new Uri("http://prohet.net/teaser/summarize.php"));
+        }
+
+        private void client_DownloadStringCompleted(object sender, DownloadStringCompletedEventArgs e)
+        {
+            if (e.Error == null)
+            {
+                string texte = e.Result;
+                MessageBox.Show(texte);
+            }
+            else
+            {
+                MessageBox.Show("Impossible de récupérer les données sur internet : " + e.Error);
+            }
+        }
+    }
+}
